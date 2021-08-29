0989d1ac0e9f4af8d29522d079c6f14f3b6a6bde
gyudon <duck9912@naver.com>
2021-08-29 16:16:28 +0900

    난처R ch6까지

--- 난.처.R/day1.R
+++ 난.처.R/day1.R
@@ -1,4 +1,4 @@
-#194 pg ì—°ìŠµë¬¸ì œ
+#194 pg ?—°?Šµë¬¸ì œ
 x <- c(2,4,6,8)
 y <- c(10, 12, 14, 16)
 z <- c(18, 20, 22, 24)
@@ -15,3 +15,53 @@ matrix(c(2,4,6,8,
          10, 12, 14, 16,
          18, 20, 22, 24),
        ncol = 3, byrow = F )
+
+
+install.packages('cowsay')
+TRUE + TRUE
+
+# 112pg ¿¬½À¹®Á¦
+#01
+a <- 7
+b <- 9
+c <- a*b ; c
+
+#02
+a <- 90 
+b <- 60
+c <- 80
+d <- 360 -(a+b+c) ; d
+
+#03
+dis <- 60
+v1 <- 20
+v2 <- 30
+(avg <- dis/v1 + dis/v2)
+
+#04
+sugar <- 25
+water <- 125
+percent <- (sugar/(water+sugar)) * 100
+print(percent)
+
+# 194pg ¿¬½À¹®Á¦
+#04
+m<- matrix(c(9,7,5,3,8,11,2,9),ncol=4,byrow = T)
+m2 <- data.frame(m)
+rownames(m2) <- c("x", "y")
+colnames(m2) <- c("a", "b", "c", "d")
+m2
+
+#08
+matrix(1:12, nrow=3) %% 3
+matrix(1:12, nrow=3) + 3
+rbind(matrix(1:9, nrow=3), c('1,','2','3')) +3 
+
+#09
+¹øÈ£ <- c(1,2,3,4,5,6,7)
+Á¦¸ñ <- c("±×´ë¶û","´ÙÅù","»¡·¡", "µÎÅë","º¸Á¶°³", "¸Åµì", "ÀÌ»óÇØ" )
+ÁÁ¾Æ¿ä <- c(16075, 8218, 12119, 738, 3200, 16144, 5110)
+love <- data.frame(¹øÈ£, Á¦¸ñ, ÁÁ¾Æ¿ä)
+
+best <- subset(love, ÁÁ¾Æ¿ä == max(love$ÁÁ¾Æ¿ä))
+best
